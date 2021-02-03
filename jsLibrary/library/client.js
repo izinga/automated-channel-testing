d451f09b71099342476459a53b4554ea8c1d1bcd
Varadekushagra <kushagra@robustest.com>
2021-02-03 18:51:46 +0530

    Client and rokuLib changes for supporting robustest run

--- jsLibrary/library/client.js
+++ jsLibrary/library/client.js
@@ -13,128 +13,156 @@
 //limitations under the License.
 //////////////////////////////////////////////////////////////////////////
 
-const axios = require('axios')
-const BASE_URL = "http://localhost:9000/v1/session"
-
+require("log-timestamp");
+const axios = require("axios");
+const BASE_URL = "http://<robustestIP>:<PORT>/roku/v1/session";
 
 class Client {
+  constructor(ip, timeout, delay, capability = {}) {
+    this.ip = ip;
+    this.timeout = timeout;
+    this.delay = delay;
+    this.sessionId = "";
+    this.capability = capability;
+    this.capability["ip"] = ip;
+    this.capability["timeout"] = timeout;
+    this.capability["delay"] = delay;
+    this.capability["sessionId"] = "";
+  }
 
-    constructor(ip, timeout, delay) {
-        this.ip = ip;
-        this.timeout = timeout;
-        this.delay = delay;
-        this.sessionId = "";
-    }
-    
-    async createSession(ip, timeout, delay) {
-        return await this.doRequest('', 'post', {'ip' : ip, 'timeout': timeout, 'pressDelay': delay}, {}, false);
-    }
+  async createSession(ip, timeout, delay) {
+    return await this.doRequest("", "post", this.capability, {}, false);
+  }
 
-    async launch(channel_code, contentID, contentType) {
-        return await this.doRequest('/launch', 'post', {'channelId' : channel_code, 'contentId': contentID, 'contentType': contentType});
-    }
+  async launch(channel_code, contentID, contentType) {
+    return await this.doRequest("/launch", "post", {
+      channelId: channel_code,
+      contentId: contentID,
+      contentType: contentType
+    });
+  }
 
-    async sendKeypress(key) {
-        return await this.doRequest('/press', 'post', {'Button' : key});
-    }
+  async sendKeypress(key) {
+    return await this.doRequest("/press", "post", { Button: key });
+  }
 
-    async deleteSession() {
-        return await this.doRequest('', 'delete');
-    }
+  async deleteSession() {
+    return await this.doRequest("", "delete");
+  }
 
-    async getDeviceInfo() {
-        return await this.doRequest('', 'get');
-    }
+  async getDeviceInfo() {
+    return await this.doRequest("", "get");
+  }
 
-    async getApps() {
-        return await this.doRequest('/apps', 'get');
-    }
+  async getApps() {
+    return await this.doRequest("/apps", "get");
+  }
 
-    async getCurrentApp() {
-        return await this.doRequest('/current_app', 'get');
-    }
+  async getCurrentApp() {
+    return await this.doRequest("/current_app", "get");
+  }
 
-    async sendSequence(sequence) {
-        return await this.doRequest('/press', 'post', {'button_sequence' : sequence});
-    }
+  async sendSequence(sequence) {
+    return await this.doRequest("/press", "post", {
+      button_sequence: sequence
+    });
+  }
 
-    async sendInputData(channelId, contentID, mediaType) {
-        return await this.doRequest('/input', 'post', {'channelId': channelId, 'contentId' : contentID, 'contentType': mediaType });
-    }
+  async sendInputData(channelId, contentID, mediaType) {
+    return await this.doRequest("/input", "post", {
+      channelId: channelId,
+      contentId: contentID,
+      contentType: mediaType
+    });
+  }
 
-    async getPlayerInfo() {
-        return await this.doRequest('/player', 'get');
-    }
+  async getPlayerInfo() {
+    return await this.doRequest("/player", "get");
+  }
 
-    async getScreenSource() {
-        return await this.doRequest('/source', 'get');
-    }
+  async getScreenSource() {
+    return await this.doRequest("/source", "get");
+  }
 
-    async getActiveElement() {
-        return await this.doRequest('/element/active', 'post');
-    }
+  async getActiveElement() {
+    return await this.doRequest("/element/active", "post");
+  }
 
-    async setTimeouts( timeoutType, delay) {
-        return await this.doRequest('/timeouts', 'post',  {'type': timeoutType, 'ms': delay});
-    }
+  async setTimeouts(timeoutType, delay) {
+    return await this.doRequest("/timeouts", "post", {
+      type: timeoutType,
+      ms: delay
+    });
+  }
 
-    async sendInstallChannel(channelCode) {
-        return await this.doRequest('/install', 'post', {'channelId' : channelCode});
-    }
+  async sendInstallChannel(channelCode) {
+    return await this.doRequest("/install", "post", { channelId: channelCode });
+  }
 
-    async getUiElements(data) {
-        return await this.doRequest('/elements', 'post', data);
-    }
+  async getUiElements(data) {
+    return await this.doRequest("/elements", "post", data);
+  }
 
-    async getUiElement(data) {
-        return await this.doRequest('/element', 'post', data);
-    }
+  async getUiElement(data) {
+    return await this.doRequest("/element", "post", data);
+  }
 
-    async sideLoadChannel(form) {
-        console.log
-        return await this.doRequest('/load', 'post', form, form.getHeaders());
-    }
+  async sideLoadChannel(form) {
+    return await this.doRequest("/load", "post", form, form.getHeaders());
+  }
 
-    async addSessionId(url) {
-        if (this.sessionId == '') {
-            let result = await this.createSession(this.ip, this.timeout, this.delay);
-            if (result.status == 200) {
-                this.sessionId = result.data.sessionId;
-            }
-        }
-        url = `/${this.sessionId}${url}`;
-        return url;
+  async addSessionId(url) {
+    if (this.sessionId == "") {
+      console.info(
+        "No exisiting session found, going to start new session",
+        url
+      );
+      let result = await this.createSession(this.ip, this.timeout, this.delay);
+      if (result.status == 200) {
+        this.sessionId = result.data.sessionId;
+      }
+    } else {
+      console.info("There is already a sesssion ", this.sessionId);
     }
+    url = `/${this.sessionId}${url}`;
+    console.log("URL ", url);
+    return url;
+  }
 
-    async doRequest(url, method, body = {}, headers = {}, addSessionId = true) {
-        try {
-            if (addSessionId == true) {
-                url = await this.addSessionId(url);
-            }
-            const result = await axios({
-                    method: method,
-                    url: `${BASE_URL}${url}`,
-                    data: body,
-                    headers: {
-                        ...headers
-                    }
-                });
-            return result;
-        } catch (errorResponse) {    
-            const response = errorResponse.response;
-            if (response == undefined) {
-                throw new Error('Could not get any response');
-            }
-            const status = response.status;
-            let errorMessage;
-            if (status == 400) {
-                errorMessage = response.data;
-            } else {
-                errorMessage = response.data.value.message;
-            }
-            throw new Error(errorMessage);
-        }
+  async doRequest(url, method, body = {}, headers = {}, addSessionId = true) {
+    try {
+      if (addSessionId == true) {
+        url = await this.addSessionId(url);
+      }
+
+      const result = await axios({
+        method: method,
+        url: `${BASE_URL}${url}`,
+        data: body,
+        headers: {
+          ...headers
+        },
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity
+      });
+
+      return result;
+    } catch (errorResponse) {
+      console.error("Error ", errorResponse);
+      const response = errorResponse.response;
+      if (response == undefined) {
+        throw new Error("Could not get any response");
+      }
+      const status = response.status;
+      let errorMessage;
+      if (status == 400) {
+        errorMessage = response.data;
+      } else {
+        errorMessage = response.data.value.message;
+      }
+      throw new Error(errorMessage);
     }
+  }
 }
 
-module.exports.Client = Client
+module.exports.Client = Client;
